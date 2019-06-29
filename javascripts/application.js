6b40181fb7dda5971708289a19aa4dbed9f85510
Linus Phan <phanlinus@gmail.com>
2019-06-28 18:05:36 -0700

    create Handlebars templates, Backbone product model, and render to template

---
+++ javascripts/application.js
@@ -0,0 +1,72 @@
+const templates = {};
+
+$("[type='text/x-handlebars']").each(function () {
+  const $template = $(this);
+
+  templates[$template.attr('id')] = Handlebars.compile($template.html());
+});
+
+
+function formatDatetime(datetime) {
+  const year = datetime.getFullYear();
+  const month = datetime.getMonth() + 1;
+  const date = datetime.getDate();
+  const hours = datetime.getHours();
+  const minutes = datetime.getMinutes();
+  const seconds = datetime.getSeconds();
+
+  return `${year}-${month}-${date}T${hours}:${minutes}:${seconds}`;
+}
+
+function formatDate(datetime) {
+  const months = [
+    'Jan', 'Feb', 'Mar',
+    'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep',
+    'Oct', 'Nov', 'Dec',
+  ];
+
+  const suffixOverrides = ['st', 'nd', 'rd'];
+  const dateSuffix = 'th';
+
+  const year = datetime.getFullYear();
+  const month = datetime.getMonth() + 1;
+  const date = datetime.getDate();
+  const hours = datetime.getHours();
+  const minutes = datetime.getMinutes();
+  const seconds = datetime.getSeconds();
+  
+  if (date <= 3) {
+    dateSuffix = suffixOverrides[date-1];
+  }
+
+  return `${months[month]} ${date}${dateSuffix}, ${year} ` +
+    `${hours}:${minutes}:${seconds}`;  
+}
+
+const ProductModel = Backbone.Model.extend({
+  setDatetime: function () {
+    const datetime = new Date(this.get('date'));
+
+    this.set('datetime', formatDatetime(datetime));
+  },
+
+  setDateFormatted: function () {
+    const datetime = new Date(this.get('date'));
+
+    this.set('dateFormatted', formatDate(datetime));
+  },
+
+  initialize: function () {
+    this.setDatetime();
+    this.setDateFormatted();
+  }
+});
+
+const product = new ProductModel(productJSON);
+
+function renderProduct() {
+  $('article').html(templates.product(product.toJSON()));
+}
+
+renderProduct();
